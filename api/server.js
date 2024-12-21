65085a4a9884ee6670c4568796e4da347356f7d0
al3xpisani <alexandre.pisani.ant@gmail.com>
2024-12-21 12:29:23 -0300

    json 200k  records  category json

--- api/server.js
+++ api/server.js
@@ -4,15 +4,15 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 
 // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('ICategory_200k_records.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
 
 // Comment out to allow write operations
-const router = jsonServer.router('ICategory_200k_records.json')
+// const router = jsonServer.router('ICategory_200k_records.json')
 
 const middlewares = jsonServer.defaults()
 
